d0ff84a37c035a3cd2e9dea0ea48026d00ebe61e
Anuj Rao <anuj.rao@myntra.com>
2021-03-17 06:07:22 +0530

    added databases structure

---
+++ datasources/mysql/users_db/users_db.go
@@ -0,0 +1,8 @@
+package users_db
+
+/*
+	this reason for having this kind of the structure is that in the "datasources" folder we can have
+	different different types of the datasources for a microservies like "mysql", "nosql", "cassaderra" etc.
+	and under a database we can have different tabels like "users" "e-mails" data etc etc.
+	so this pattern is scalable.
+*/
